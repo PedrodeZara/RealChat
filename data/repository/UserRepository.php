0fee39456faac0621f7f6f5c37e3d3573facf10d
Pedro Zara <zarapedrohenrique@gmail.com>
2026-01-11 17:10:17 -0300

    Adição de classe intercessora UserRepository

---
+++ data/repository/UserRepository.php
@@ -0,0 +1,68 @@
+<?php 
+require_once "../database/connexion.php";
+require_once "../models/user.php";
+
+class UserRepository {
+    private PDO $pdo;
+
+    public function __construct() {
+        $this->pdo = Database::getConnection();
+    }
+
+    public function getById(int $id): ?array {
+        $stmt = $this->pdo->prepare("SELECT * FROM user WHERE id = :id");
+        $stmt->execute([":id" => $id]);
+        $stmt->fetch(PDO::FETCH_ASSOC);
+
+        try {
+            return $stmt ?: null;
+        }
+        catch (PDOException $e) {
+            echo $e
+        }
+    }
+
+    public function insert(User $user): bool {
+        $stmt = $this->pdo->prepare("INSERT INTO user (nome, descricao, telefone) VALUES (:nome,:descricao,:telefone)");
+        
+        try {            
+            return $stmt->execute([
+                ":nome" => $user->getNome(),
+                ":descricao" => $user->getDescricao(),
+                ":telefone" => $user->getTelefone()
+            ]);
+        }
+        catch (PDOException $e) {
+            echo $e
+            return false;
+        }
+    }
+
+    public function update(User $user, $id): bool {
+        $stmt = $this->pdo->prepare("UPDATE user SET nome = :nome, descricao = :descricao WHERE id = :id");
+        
+        try {            
+            return $stmt->execute([
+                ":id" => $id,
+                ":nome" => $user->getNome(),
+                ":descricao" => $user->getDescricao(),
+            ]);
+        }
+        catch (PDOException $e) {
+            echo $e
+            return false;
+        }
+    }
+
+    public function delete($id): bool {
+        $stmt = $this->pdo->prepare("DELETE FROM user WHERE id = :id");
+        try {
+            $stmt->execute(["id" => $id]);
+        }
+        catch(PDOException $e) {
+            echo $e;
+            return false;
+        } 
+    } 
+
+}
