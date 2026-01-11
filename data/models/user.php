9a993e6df8bc869ae269593b2942a7f59c3634d8
Pedro Zara <zarapedrohenrique@gmail.com>
2026-01-11 16:03:03 -0300

    Adição de classe e reorganização de código

---
+++ data/models/user.php
@@ -0,0 +1,39 @@
+<?php
+
+class User {
+    private int $id;
+    private string $nome;
+    private string $descricao;
+    private string $telefone;
+
+    public function __construct( int $id, string $nome, string $descricao, string $telefone) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+        $this->telefone = $telefone;
+    }
+
+    public function getName(): string {
+        return $this->nome;
+    }
+
+    public function setName($nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao(): string {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao): void {
+        $this->descricao = $descricao;
+    }
+
+    public function getTelefone(): string {
+        return $this->telefone;
+    }
+
+    public function setDescricao($telefone): void {
+        $this->telefone = $telefone;
+    }
+}
