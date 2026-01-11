0aa00f5ee2f41e46d1ca94aa8dd6d93d8b5279b3
Pedro Zara <zarapedrohenrique@gmail.com>
2026-01-11 15:38:06 -0300

    Adição de coneção PHP

---
+++ data/controller/connexion.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database {
+    private static $pdo;
+
+    public static function getConnection() {
+        if (self::$pdo == null) {
+            $host = "mysql";
+            $dbname = "RealChat";
+            $user = "root";
+            $pass = "root";
+
+            try {
+                self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;",$user,$pass);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Conexão realizada";
+            }
+
+            catch (PDOException $ex) {
+                echo ex;
+            }
+        }
+        return self::$pdo;
+    }
+}
